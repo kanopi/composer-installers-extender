eb408aeeb482686793af531746d6f3af529e4bce
Sean Dietrich <sean@kanopistudios.com>
2019-02-14 15:12:09 -0800

    Added change to allow for web-root to be customized

--- src/InstallerHelper.php
+++ src/InstallerHelper.php
@@ -12,4 +12,45 @@ class InstallerHelper extends BaseInstaller {
 		return array( false );
 	}
 
+	/**
+	 * Return the install path based on package type.
+	 *
+	 * @param  PackageInterface $package
+	 * @param  string           $frameworkType
+	 * @return string
+	 */
+	public function getInstallPath(PackageInterface $package, $frameworkType = '')
+	{
+			$type = $this->package->getType();
+			$prettyName = $this->package->getPrettyName();
+			if (strpos($prettyName, '/') !== false) {
+					list($vendor, $name) = explode('/', $prettyName);
+			} else {
+					$vendor = '';
+					$name = $prettyName;
+			}
+			$availableVars = $this->inflectPackageVars(compact('name', 'vendor', 'type', 'web-dir'));
+			$extra = $package->getExtra();
+			if (!empty($extra['installer-name'])) {
+					$availableVars['name'] = $extra['installer-name'];
+			}
+			$availableVars['web-dir'] = $extra['web-dir'];
+
+			if ($this->composer->getPackage()) {
+					$extra = $this->composer->getPackage()->getExtra();
+					if (!empty($extra['installer-paths'])) {
+							$customPath = $this->mapCustomInstallPaths($extra['installer-paths'], $prettyName, $type, $vendor);
+							if ($customPath !== false) {
+									return $this->templatePath($customPath, $availableVars);
+							}
+					}
+			}
+			$packageType = substr($type, strlen($frameworkType) + 1);
+			$locations = $this->getLocations();
+			if (!isset($locations[$packageType])) {
+					throw new \InvalidArgumentException(sprintf('Package type "%s" is not supported', $type));
+			}
+			return $this->templatePath($locations[$packageType], $availableVars);
+	}
+
 }
